5211a7b2847a97a4724ec2a049cfb78b353cf2f0
Kevin Finn <kevin@relevant.healthcare>
2018-09-19 01:39:45 -0400

    Copy ActiveModel + React props from analytics

--- lib/active_model_react_props.rb
+++ lib/active_model_react_props.rb
@@ -1,2 +1,17 @@
 module ActiveModelReactProps
+  def self.props_for(object, **options)
+    if object.is_a?(Enumerable)
+      object.map { |obj| render_json_partial(obj, **options) }
+    else
+      JbuilderTemplate.new(self) do |json|
+        json.partial! partial: object, formats: [:json], **options
+      end.attributes!
+    end
+  end
+
+  def self.errors_for(model)
+    model.errors.map do |attribute, message|
+      { attribute: attribute, message: message }
+    end
+  end
 end
